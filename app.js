a518d26e293b28f0d1034d6896c96b356afc41c9
i5ting <i5ting@126.com>
2017-10-30 11:33:36 +0800

    refact, please ignore

--- app.js
+++ app.js
@@ -2,19 +2,17 @@ var path = require('path')
 var express = require('express')
 var app = express()
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 // view engine setup
-app.engine('art', require('express-art-template'));
-
+app.engine('art', require('express-art-template'))
 app.set('view options', {
     debug: process.env.NODE_ENV !== 'production'
-});
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'art');
+})
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'art')
 
+// routes
 app.get('/', function (req, res) {
   res.render('index', {
       title : 'Hello Express && Art-template'
