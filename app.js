59886af322bc01aed5e59d49ded61ec3c6e497c7
i5ting <i5ting@126.com>
2017-10-30 11:24:17 +0800

    lazycommmit, please ignore

---
+++ app.js
@@ -0,0 +1,24 @@
+var path = require('path')
+var express = require('express')
+var app = express()
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// view engine setup
+app.engine('art', require('express-art-template'));
+
+app.set('view options', {
+    debug: process.env.NODE_ENV !== 'production'
+});
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'art');
+
+app.get('/', function (req, res) {
+  res.render('index', {
+      title : 'Hello Express && Art-template'
+  })
+})
+
+app.listen(3000)
